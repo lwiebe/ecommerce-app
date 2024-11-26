64b4831c46d7a7d1fe8caaa4d7600ff2064fa1d4
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2024-11-26 20:42:32 +0100

    routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
@@ -8,10 +12,18 @@ const App = () => {
       <>
         <Navbar />
         <hr />
+
+        <div className='flex w-full'>
+          <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
       </>
-      <div className='flex w-full'>
-        <Sidebar />
-      </div>
     </div>
   )
 }
