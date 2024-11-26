c0682ee4026b62bd28eef97cc3937cdd0d0c24f1
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2024-11-26 17:07:02 +0100

    functions

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,7 +38,7 @@ const addProduct = async (req, res) => {
         const product = new productModel(productData);
         await product.save()
 
-        res.json({success:true,message: "Product Added"})
+        res.json({ success: true, message: "Product Added" })
 
     } catch (error) {
         console.log(error);
@@ -49,20 +49,42 @@ const addProduct = async (req, res) => {
 
 // fonction for list product
 const listProducts = async (req, res) => {
+    try {
 
+        const products = await productModel.find({});
+        res.json({ success: true, products })
 
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // fonction for removing product
 const removeProduct = async (req, res) => {
+    try {
 
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Removed" })
 
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // fonction for single product info
 const singleProduct = async (req, res) => {
+    try {
 
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({ success: true, product })
 
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 export { listProducts, addProduct, removeProduct, singleProduct }
