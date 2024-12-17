88cf38403156c47e848506c6b4a2497cee81e8ca
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2024-12-17 08:56:12 +0100

    stripe payment method

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,10 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from 'stripe'
 
+// global variables
+const currency = 'eur'
+const deliveryCharge = 10
+
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -40,7 +44,7 @@ const placeOrder = async (req, res) => {
 // Placing orders using Stripe Method
 const placeOrderStripe = async (req, res) => {
     try {
-        
+
         const { userId, items, amount, address } = req.body;
         const { origin } = req.headers;
 
@@ -57,10 +61,61 @@ const placeOrderStripe = async (req, res) => {
         const newOrder = new orderModel(orderData)
         await newOrder.save()
 
-        
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price + 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: "Delivery Charges"
+                },
+                unit_amount: deliveryCharge + 100
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.json({ success: true, session_url: session.url });
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Verify Stripe
+const verifyStripe = async (req, res) => {
+
+    const { orderId, success, userId } = req.body
+
+    try {
+        if (success === "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            await userModel.findByIdAndUpdate(userId, { cartData: {} })
+            res.json({ success: true });
+        } else {
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({ success: false })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -102,11 +157,11 @@ const userOrders = async (req, res) => {
 // update order status from Admin Panel
 const updateStatus = async (req, res) => {
     try {
-        
+
         const { orderId, status } = req.body
 
-        await orderModel.findByIdAndUpdate(orderId, {status})
-        res.json({success:true,message:'Status Updated'})
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: 'Status Updated' })
 
     } catch (error) {
         console.log(error);
@@ -114,4 +169,4 @@ const updateStatus = async (req, res) => {
     }
 }
 
-export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
+export { verifyStripe ,placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -58,12 +58,22 @@ const PlaceOrder = () => {
 
         // API Calls for COD
         case 'cod':
-          const response = await axios.post(backendUrl + '/api/order/place',orderData,{headers:{token}})
+          const response = await axios.post(backendUrl + '/api/order/place', orderData, { headers: { token } })
           if (response.data.success) {
             setCartItems({})
             navigate('/orders')
           } else {
             toast.error(response.data.message)
+          }
+          break;
+
+        case 'stripe':
+          const responseStripe = await axios.post(backendUrl + '/api/order/stripe', orderData, { headers: { token } })
+          if (responseStripe.data.success) {
+            const {session_url} = responseStripe.data
+            window.location.replace(session_url)
+          } else {
+            toast.error(responseStripe.data.message)
           }
           break;
 
